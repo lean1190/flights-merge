32a00b3971eaa50e21bb18d96e1f0d6f9cb1b495
Lean <leian1306@gmail.com>
2023-01-13 18:33:39 +0100

    src flights constants: move the flight sources to an array

--- src/flights/constants.ts
+++ src/flights/constants.ts
@@ -1,2 +1,7 @@
 export const flightsSourceUrl1 = "https://coding-challenge.powerus.de/flight/source1";
 export const flightsSourceUrl2 = "https://coding-challenge.powerus.de/flight/source2";
+
+export const flightSources = [
+    'https://coding-challenge.powerus.de/flight/source1',
+    'https://coding-challenge.powerus.de/flight/source2',
+];
