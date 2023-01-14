db545799ed0c3589509939505966861ba2ea6672
Lean <leian1306@gmail.com>
2023-01-14 12:31:33 +0100

    src cache interfaces: add initial generic cache interface

---
+++ src/cache/interfaces/cache.interface.ts
@@ -0,0 +1,12 @@
+import { Observable } from 'rxjs';
+
+export interface Cache<T> {
+    set(key: string, value: T, params?: CacheParams): Observable<void>;
+    get(key: string): Observable<T>;
+    remove(key: string): Observable<void>;
+    clear(): Observable<void>;
+}
+
+export interface CacheParams {
+    ttlSeconds?: number;
+}
