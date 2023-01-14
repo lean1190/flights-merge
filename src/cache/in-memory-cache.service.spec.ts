be21d4467b07409a907694ada8a2aea42b1277e9
Lean <leian1306@gmail.com>
2023-01-14 12:36:36 +0100

    src cache in memory cache: implement empty Cache interface

--- src/cache/in-memory-cache.service.spec.ts
+++ src/cache/in-memory-cache.service.spec.ts
@@ -3,14 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { InMemoryCacheService } from './in-memory-cache.service';
 
 describe('InMemoryCacheService', () => {
-    let service: InMemoryCacheService;
+    let service: InMemoryCacheService<any>;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [InMemoryCacheService],
         }).compile();
 
-        service = module.get<InMemoryCacheService>(InMemoryCacheService);
+        service = module.get<InMemoryCacheService<any>>(InMemoryCacheService);
     });
 
     it('should be defined', () => {
--- src/cache/in-memory-cache.service.ts
+++ src/cache/in-memory-cache.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+import { Cache, CacheParams } from './interfaces/cache.interface';
 
 @Injectable()
-export class InMemoryCacheService {}
+export class InMemoryCacheService<T> implements Cache<T> {
+
+    private cacheStore: { [key: string]: T } = {};
+
+    public set(key: string, value: T, params?: CacheParams | undefined): Observable<void> {
+        throw new Error('Method not implemented.');
+    }
+
+    public get(key: string): Observable<T> {
+        throw new Error('Method not implemented.');
+    }
+
+    public remove(key: string): Observable<void> {
+        throw new Error('Method not implemented.');
+    }
+
+    public clear(): Observable<void> {
+        throw new Error('Method not implemented.');
+    }
+}
